a1f86ab85fb3648e4d28c04f1dc9884025f0700f
François MICAUX <fmicaux_at_actilis_._net>
2018-06-02 16:38:22 +0200

    Update base image to httpd-cgi:latest

---
+++ getpdf-ep.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+chgrp -R apache    /var/www/html \
+   && chown -R apache    /var/www/html/logs /var/www/html/docs \
+   && rm -f /var/www/html/index.*
+
+
+echo "$@"
+exec /httpd-entrypoint.sh "$@"
